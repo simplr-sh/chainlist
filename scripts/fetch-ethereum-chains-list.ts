038f51abdc5066884b802aa1e12bd3350ecc7eaf
simplr-sh <simplr.sh@gmail.com>
2024-12-29 15:37:22 +0530

    Add new Ethereum chains and update fetch scripts for chain data

--- scripts/fetch-ethereum-chains-list.ts
+++ scripts/fetch-ethereum-chains-list.ts
@@ -2,16 +2,18 @@ async function fetchEthereumChainsList() {
   const response = await fetch('https://chainid.network/chains.json').then(
     (res) => res.json()
   )
-  const chains = response.length
-  console.log(chains)
+  const chains = response
+  console.log(chains.length)
+  Bun.write('./src/data/chainlist.json', JSON.stringify(response, null, 2))
 }
 
 async function fetchEthereumChainsMiniList() {
   const response = await fetch('https://chainid.network/chains_mini.json').then(
     (res) => res.json()
   )
-  const chains = response.length
-  console.log(chains)
+  const chains = response
+  console.log(chains.length)
+  Bun.write('./src/data/chainlist_mini.json', JSON.stringify(response, null, 2))
 }
 
 fetchEthereumChainsList()
